f7bcbbc7b8811677f9e57039936a9f190955794f
Jess2 <jess2.developer@gmail.com>
2020-12-28 22:30:51 +0900

    feat: add header & list component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Style from 'components/Header.module.scss';
+
+console.log(Style)
 
 export default function Header() {
     return (
-        <div className="header">
-            <header>Header</header>
-        </div>
+        <header className={`${Style.header}`}>
+            <div>"</div>
+            <div>오</div>
+            <div>늘</div>
+            <div>의</div>
+            <div>&nbsp;</div>
+            <div>기</div>
+            <div>분</div>
+            <div>"</div>
+        </header>
     );
 }
